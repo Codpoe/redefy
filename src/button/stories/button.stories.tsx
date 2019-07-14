bf56a23b86a2cfeabd7b2dc122ce6534d7e07bed
codpoe <codpoe.me@gmail.com>
2019-07-14 13:06:23 +0800

    [doc] Button

--- src/button/stories/button.stories.tsx
+++ src/button/stories/button.stories.tsx
@@ -8,52 +8,52 @@ import { Search, Upload, Download } from '../../icon/index';
 storiesOf('Basic | Button', module)
   .add('基础用法', () => (
     <>
-      <Button onClick={action('clicked')}>默认按钮</Button>{' '}
-      <Button type="primary">主要按钮</Button>{' '}
-      <Button type="success">成功按钮</Button>{' '}
-      <Button type="warning">警告按钮</Button>{' '}
-      <Button type="error">危险按钮</Button>
+      <Button onClick={action('clicked')}>Default</Button>{' '}
+      <Button type="primary">Primary</Button>{' '}
+      <Button type="success">Success</Button>{' '}
+      <Button type="warning">Warning</Button>{' '}
+      <Button type="error">Error</Button>
     </>
   ))
   .add('镂空 hollow', () => (
     <>
-      <Button hollow>默认按钮</Button>{' '}
+      <Button hollow>Hollow</Button>{' '}
       <Button type="primary" hollow>
-        边框按钮
+        Hollow
       </Button>{' '}
       <Button type="success" hollow>
-        边框按钮
+        Hollow
       </Button>{' '}
       <Button type="warning" hollow>
-        边框按钮
+        Hollow
       </Button>{' '}
       <Button type="error" hollow>
-        边框按钮
+        Hollow
       </Button>
     </>
   ))
   .add('文字 text', () => (
     <>
-      <Button text>默认按钮</Button>{' '}
+      <Button text>Text</Button>{' '}
       <Button type="primary" text>
-        文字按钮
+        Text
       </Button>{' '}
       <Button type="success" text>
-        文字按钮
+        Text
       </Button>{' '}
       <Button type="warning" text>
-        文字按钮
+        Text
       </Button>{' '}
       <Button type="error" text>
-        文字按钮
+        Text
       </Button>
     </>
   ))
   .add('两边圆形 round', () => (
     <>
-      <Button round>圆形按钮</Button>{' '}
+      <Button round>Round</Button>{' '}
       <Button type="primary" hollow round>
-        圆形按钮
+        Round
       </Button>
     </>
   ))
@@ -74,69 +74,68 @@ storiesOf('Basic | Button', module)
     <>
       <Button>
         <Upload />
-        &nbsp;上传
+        &nbsp;Upload
       </Button>{' '}
       <Button hollow type="primary">
         <Download />
-        &nbsp;下载
+        &nbsp;Download
       </Button>
     </>
   ))
   .add('尺寸 size', () => (
     <>
-      <Button size="large">大型按钮</Button>{' '}
-      <Button size="normal">普通按钮</Button>{' '}
-      <Button size="small">小型按钮</Button>
+      <Button size="large">Large</Button> <Button size="normal">Normal</Button>{' '}
+      <Button size="small">Small</Button>
     </>
   ))
   .add('无 padding 纯净', () => (
     <>
-      <Button pure>默认按钮</Button>{' '}
+      <Button pure>Pure</Button>{' '}
       <Button type="primary" pure>
-        纯净按钮
+        Pure
       </Button>{' '}
       <Button type="success" pure>
-        纯净按钮
+        Pure
       </Button>{' '}
       <Button type="warning" pure>
-        纯净按钮
+        Pure
       </Button>{' '}
       <Button type="error" pure>
-        纯净按钮
+        Pure
       </Button>
     </>
   ))
   .add('禁用 disabled', () => (
     <>
-      <Button disabled>禁用按钮</Button>{' '}
+      <Button disabled>Disabled</Button>{' '}
       <Button type="primary" disabled>
-        禁用按钮
+        Disabled
       </Button>{' '}
       <Button hollow type="error" disabled>
-        禁用按钮
+        Disabled
       </Button>{' '}
       <Button text disabled>
-        禁用按钮
+        Disabled
       </Button>
     </>
   ))
   .add('链接', () => (
     <Button href="http://t.tt" target="_blank" type="primary">
-      链接按钮
+      Smartisan
     </Button>
   ))
   .add('块级按钮 block', () => <Button block>块级按钮</Button>)
   .add('组合', () => (
     <>
       <ButtonGroup>
-        <Button>过去</Button>
-        <Button>现在</Button>
-        <Button>未来</Button>
+        <Button>Past</Button>
+        <Button>Present</Button>
+        <Button>Future</Button>
       </ButtonGroup>{' '}
       <ButtonGroup type="primary" hollow>
-        <Button>过去</Button>
-        <Button>现在</Button>
-        <Button>未来</Button>
+        <Button>Past</Button>
+        <Button>Present</Button>
+        <Button>Future</Button>
       </ButtonGroup>
     </>
   ));
