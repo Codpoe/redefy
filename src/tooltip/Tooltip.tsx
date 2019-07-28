673c7a94eea5d2baf6696f6ecf9cbf9d395f4642
codpoe <codpoe.me@gmail.com>
2019-07-29 01:47:33 +0800

    [improvement] Tooltip: 调整延迟

--- src/tooltip/Tooltip.tsx
+++ src/tooltip/Tooltip.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import Pop, { PopProps } from '../pop/index';
 import bem from '../utils/bem';
-import './styles/index.css';
+import './styles/tooltip.css';
 
 const b = bem('x-tooltip');
 
@@ -10,6 +10,7 @@ export type TooltipProps = PopProps;
 
 const Tooltip: React.SFC<TooltipProps> = (props: TooltipProps) => (
   <Pop
+    delay={[300, 150]}
     {...props}
     className={cx(b(), props.className)}
     contentClassName={cx(b('content'), props.contentClassName)}
