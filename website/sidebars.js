b54c30d1ce9c19a238037e288432983c4fbf044e
codpoe <codpoe.me@gmail.com>
2019-12-28 17:51:56 +0800

    docs(Form): add doc

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   docs: {
     ['开发指南']: ['introduction', 'quick-start', 'changelog'],
     ['基础组件']: ['button', 'icon'],
-    ['表单组件']: ['checkbox', 'radio', 'switch'],
+    ['表单组件']: ['checkbox', 'form', 'radio', 'switch'],
   },
 };
