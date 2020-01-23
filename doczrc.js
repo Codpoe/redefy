1517bca789ce146f0522dacc2f9d93923fa09239
codpoe <codpoe.me@gmail.com>
2020-01-24 03:29:26 +0800

    update docz

--- doczrc.js
+++ doczrc.js
@@ -4,12 +4,27 @@ export default {
   typescript: true,
   propsParser: false,
   themeConfig: {
+    showPlaygroundEditor: false,
     showDarkModeSwitch: false,
+    styles: {
+      root: {
+        fontSize: 1,
+      },
+      inlineCode: {
+        margin: '0 2px',
+        padding: '2px 6px',
+        borderRadius: '3px',
+        backgroundColor: '#eceff2',
+      },
+    },
   },
   menu: [
     {
       name: '开发指南',
       menu: ['介绍', '快速上手', '更新日志'],
     },
+    '基础组件',
+    '表单组件',
+    '交互组件',
   ],
 };
